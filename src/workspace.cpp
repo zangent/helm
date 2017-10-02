ab8f569b3ecb866871b1117183e0dc90ba2f6918
Zachary Pierson <zacpiersonhehe@gmail.com>
2017-10-02 08:34:44 -0500

    Beginning to flesh out Workspace

--- src/workspace.cpp
+++ src/workspace.cpp
@@ -3,10 +3,29 @@ enum Workspace_Event {
 	PARSED, TYPE_CHECKED, IR, COMPILED
 };
 
+struct Workspace;
+
+typedef void (*Workspace_Event_Proc)(Workspace*);
+
 struct Workspace {
 	String name;
 	String main;
 	Parser parser = {0};
 	Checker checker = {0};
-
+	Array<Workspace_Event_Proc> event_handlers;
 };
+
+Workspace workspace_create() {
+	Workspace w;
+	array_init(&w.event_handlers, heap_allocator());
+	return w;
+}
+
+void workspace_compile() {
+
+}
+
+// Necessary so that Odin-level code can interface with the Array class.
+void workspace_add_event_handler(Workspace *w, Workspace_Event_Proc wep) {
+	w->event_handlers()
+}
